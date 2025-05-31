739d596da2b601751769bd88249292075e844b38
Colbyn Wadman <hello@colbyn.com>
2025-05-30 21:58:20 -0600

    update static site code

--- scripts/watch-compiler.sh
+++ scripts/watch-compiler.sh
@@ -1,5 +1,5 @@
 set -e
 
 # watchexec --ignore output/ -w . -e html -- scripts/compile.sh
-watchexec --ignore-file .watchexec-ignore -w sample -e html -e css -- scripts/compile.sh
+watchexec --ignore-file .watchexec-ignore -w sample -e html -e css -- scripts/compile-sample-site.sh
 
